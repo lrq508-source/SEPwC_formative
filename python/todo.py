d706b98347ea151f9b9560fcfd4ce9029890369b
lrq508-source <lrq508@york.ac.uk>
2026-04-17 18:30:08 +0200

    fixing list_task, add_task, remove_task

--- python/todo.py
+++ python/todo.py
@@ -1,38 +1,59 @@
+"""A simple command-line to-do list application."""
 import argparse
-import os
 
 TASK_FILE = ".tasks.txt"
 
+
 def add_task(task):
-    """Function: add_task
-    
+    """Add a new task to the task file.
+
     Input - a task to add to the list
     Return - nothing
     """
+    task_file = open(TASK_FILE, "a")
+    task_file.write(task + "\n")
+    task_file.close()
+
 
 def list_tasks():
-    return
+    """List all tasks from the task file."""
+    task_file = open(TASK_FILE, "r")
+    tasks = task_file.read().splitlines()
+    task_file.close()
+
+    result = ""
+    number = 1
+    for task in tasks:
+        result = result + str(number) + ". " + task + "\n"
+        number = number + 1
+
+    result = result.strip()
+    return result
 
 
 def remove_task(index):
-    return
+    """Remove a task by its number from the task file."""
+    task_file = open(TASK_FILE, "r")
+    tasks = task_file.read().splitlines()
+    task_file.close()
+
+    position = index - 1
+
+    if position >= 0 and position < len(tasks):
+        tasks.pop(position)
+
+    task_file = open(TASK_FILE, "w")
+    for task in tasks:
+        task_file.write(task + "\n")
+    task_file.close()
+
 
 def main():
+    """Run the command-line todo app."""
     parser = argparse.ArgumentParser(description="Command-line Todo List")
-    parser.add_argument(
-            "-a",
-            "--add",
-            help="Add a new task"
-            )
-    parser.add_argument(
-            "-l",
-            "--list",
-            action="store_true",
-            help="List all tasks")
-    parser.add_argument(
-            "-r",
-            "--remove",
-            help="Remove a task by index")
+    parser.add_argument("-a", "--add", help="Add a new task")
+    parser.add_argument("-l", "--list", action="store_true", help="List all tasks")
+    parser.add_argument("-r", "--remove", help="Remove a task by index")
 
     args = parser.parse_args()
 
@@ -49,3 +70,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
